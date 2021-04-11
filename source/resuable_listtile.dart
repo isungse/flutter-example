9a2458bf9899d905c0770b5e4a2d52f574366059
isungse <77379891+isungse@users.noreply.github.com>
2021-04-11 20:24:21 +0900

    Add files via upload

--- source/resuable_listtile.dart
+++ source/resuable_listtile.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
 
+class ClinicScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          title: Text('진료과장 / 외래'),
+        ),
+        body: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            ListTileWidget(
+              title: 'test1',
+            ),
+            ListTileWidget(
+              title: 'test2',
+            ),
+            ListTileWidget(
+              title: 'test3',
+            ),
+          ],
+        ));
+  }
+}
+
+class ListTileWidget extends StatelessWidget {
+  final String title;
+
+  const ListTileWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.person),
+      title: Text(
+        title,
+        style: TextStyle(
+            fontSize: 17.0, fontWeight: FontWeight.w700, color: Colors.black87),
+      ),
+    );
+  }
+}
