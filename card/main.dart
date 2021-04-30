64d875cc6e7e33c475f133e882b5e63ffb047118
isungse <77379891+isungse@users.noreply.github.com>
2021-04-30 16:35:42 +0900

    Update main.dart

--- card/main.dart
+++ card/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widget_app/transaction.dart';
+import 'package:intl/intl.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -22,59 +24,83 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
     Transaction(
-      id: 't2',
+      id: 't1',
       title: 'Weekly Groceries',
       amount: 16.53,
       date: DateTime.now(),
     ),
   ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
               color: Colors.blue,
-              child: Text('CHART!'),
+              child: Container(
+                width: double.infinity,
+                child: Text('CHART'),
+              ),
               elevation: 5,
             ),
-          ),
-          Column(
-            children: transaction.map((tx) {
-              return Card(
+            Column(
+              children: transaction.map((tx) {
+                return Card(
                   child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      tx.amount.toString(),
-                    ),
-                  ),
-                  Column(
                     children: <Widget>[
-                      Text(tx.title),
-                      Text(
-                        tx.date.toString(),
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
                       ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ));
-            }).toList(),
-          ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-          ),
-        ],
-      ),
-    );
+                  ),
+                );
+              }).toList(),
+            ),
+            Card(
+              color: Colors.red,
+              child: Text('LIST OF TX'),
+            )
+          ],
+        ));
   }
 }
+
